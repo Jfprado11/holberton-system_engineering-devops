c4d37c3d0fdf93ef2f0d95978ce01862f15151d0
Felipe Prado <jfpc11@misena.edu.co>
2021-08-14 17:41:09 -0500

    changin file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # sets up the config file
 include stdlib
-file_line { ' to the pricate key':
+file_line { 'to the pricate key':
   path    => '/etc/ssh/ssh_config',
   line    => '    IdentityFile ~/.ssh/holberton',
   replace => true,
 }
 
-file_line {' turn off password':
+file_line { 'turn off password':
   path    => '/etc/ssh/ssh_config',
   line    => '    PasswordAuthentication no',
   replace => true,
