4d4b28923f40fa8e2edb56c3dba97b039951a27f
Felipe Prado <jfpc11@misena.edu.co>
2021-09-13 19:51:05 -0500

    parsing into json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""gathering the information of an apy"""
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    person = sys.argv[1]
+    info = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(person))
+
+    user = info.json()
+    values = {"userId": user["id"]}
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos/", params=values)
+
+    todos = todos.json()
+
+    data = []
+    for todo in todos:
+        data.append([{"task": todo["title"],
+                     "completed": todo["completed"], "username": user["username"]}])
+    datas = {user["id"]: data}
+
+    with open("{}.json".format(user["id"]), "w", newline="") as file:
+        # data_json = json.dumps(datas)
+
+        json.dump(datas, file)
