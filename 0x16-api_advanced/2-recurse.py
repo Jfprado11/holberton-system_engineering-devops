a14122073b1576122cba32fe1623eab282d728fe
Felipe Prado <jfpc11@misena.edu.co>
2021-09-27 20:50:07 -0500

    recursive the hot titles

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+"""return the hot posts for subredist"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """recursive append the list"""
+    params = {"limit": 1, "after": after}
+    headers = {"User-agent": "holbie"}
+    data = requests.get("https://www.reddit.com/r/{}/hot.json".format(
+        subreddit), allow_redirects=False,
+        params=params, headers=headers)
+    if data.status_code == 200:
+        data = data.json()
+        posts = data["data"]["children"]
+        if data["data"]["dist"] == 0:
+            return(None)
+        else:
+            after = data["data"]["after"]
+            if after is not None:
+                hot_list.append(posts[0]["data"]["title"])
+                recurse(subreddit, hot_list, after)
+            else:
+                return (hot_list)
+    else:
+        return(None)
