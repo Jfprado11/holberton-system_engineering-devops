2d87825fbf6996c6818b7246ca33eeb7a56b126f
Felipe Prado <jfpc11@misena.edu.co>
2021-09-27 19:32:01 -0500

    fixing the errors

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,16 +8,15 @@ def top_ten(subreddit):
     """the ten hottest reddits"""
     params = {"q": subreddit, "sort": "hot", "limit": 10}
     headers = {"User-agent": "holbie"}
-    try:
-        data = requests.get("https://www.reddit.com/search.json",
-                            params=params, headers=headers)
-        if data.status_code == 200:
-            data = data.json()
-            posts = data["data"]["children"]
-            if data["dist"] == 0:
-                for post in posts:
-                    print(post["data"]["title"])
-            else:
-                print(None)
-    except:
+    data = requests.get("https://www.reddit.com/search.json",
+                        params=params, headers=headers)
+    if data.status_code != 200:
+        data = data.json()
+        posts = data["data"]["children"]
+        if data["dist"] == 0:
+            for post in posts:
+                print(post["data"]["title"])
+        else:
+            print(None)
+    else:
         print(None)
