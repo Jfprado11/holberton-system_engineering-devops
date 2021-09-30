1e09920405ce86b5a9af21a7c7ede8204e6d951f
Felipe Prado <jfpc11@misena.edu.co>
2021-09-30 11:44:59 -0500

    adding the different the comand

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -5,7 +5,6 @@ exec { 'restart':
 file_line {'changing':
   ensure  => 'present',
   path    => '/var/www/html/wp-settings.php',
-  replace => true,
   line    => "require_once(ABSPATH . WPINC . '/class-wp-locale.php'",
   match   => "require_once(ABSPATH . WPINC . '/class-wp-locale.phpp'",
 }
