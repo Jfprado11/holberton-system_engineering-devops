458498888b5d87f515f658736df10d7d1b138664
Felipe Prado <jfpc11@misena.edu.co>
2021-09-30 11:47:10 -0500

    in one line

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # debug the apache2
 exec { 'restart':
-path    => ['/bin', '/usr/bin', '/usr/sbin']
 command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; service apache2 restart'
+path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
